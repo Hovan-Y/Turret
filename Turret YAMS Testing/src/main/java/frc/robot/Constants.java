c118391bfed5c2220dfa8474f27a2af527c3121e
Hovan-Y <362939308@gotvdsb.ca>
2026-01-30 16:49:55 -0500

    New Pivot Subsystem (Intake Pivot)

--- Turret YAMS Testing/src/main/java/frc/robot/Constants.java
+++ Turret YAMS Testing/src/main/java/frc/robot/Constants.java
@@ -75,10 +75,14 @@ public final class Constants {
 
   public static class IntakeConstants {
     public static int IntakeMotorID = 16;
-    public static int PivotMotorID = 0;
+    public static double INTAKE_SPEED = 1.0;
   }
 
   public static class FeederConstants {
     public static int FeederMotorID = 0;
+  }
+
+  public static class PivotConstants {
+    public static int PivotMotorID = 0;
   }
 }
---
+++ Turret YAMS Testing/src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -0,0 +1,81 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Seconds;
+
+import java.util.function.Supplier;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
+
+public class PivotSubsystem extends SubsystemBase{
+    private SparkMax pivotMotor = new SparkMax(Constants.PivotConstants.PivotMotorID, MotorType.kBrushless);
+
+    private SmartMotorControllerConfig PivotSMCConfig = new SmartMotorControllerConfig() //TODO : Configure to whatever motor used
+    .withControlMode(ControlMode.CLOSED_LOOP)
+    .withClosedLoopController(25, 0, 0, DegreesPerSecond.of(360), DegreesPerSecondPerSecond.of(360))
+    .withFeedforward(new SimpleMotorFeedforward(0, 10, 0))
+    .withTelemetry("PivotMotor", TelemetryVerbosity.HIGH)
+    .withGearing(new MechanismGearing(GearBox.fromReductionStages(5, 5, 60.0 / 18.0)))
+    .withMotorInverted(false)
+    .withIdleMode(MotorMode.COAST)
+    .withSoftLimit(Degrees.of(0), Degrees.of(150))
+    .withStatorCurrentLimit(Amps.of(10))
+    .withClosedLoopRampRate(Seconds.of(0.1))
+    .withOpenLoopRampRate(Seconds.of(0.1));
+
+    private SmartMotorController pivotController = new SparkWrapper(pivotMotor, DCMotor.getNeo550(1), PivotSMCConfig);
+
+    private final ArmConfig pivotConfig = new ArmConfig(pivotController)//TODO : Ensure Hard Limit matches our Robot
+    .withSoftLimits(Degrees.of(0), Degrees.of(150))
+    .withHardLimit(Degrees.of(0), Degrees.of(155))
+    .withStartingPosition(Degrees.of(0))
+    .withLength(Feet.of(1)) //Ensure 
+    .withMass(Pounds.of(2)) 
+    .withTelemetry("Pivot", TelemetryVerbosity.HIGH);
+
+    private Arm pivot = new Arm(pivotConfig);
+
+    public PivotSubsystem() {}
+
+    public Command setAngle(Angle angle) {
+        return pivot.setAngle(angle).withName("Pivot.SetAngle");
+    }
+
+    public Command setAngleDynamic(Supplier<Angle> angleSupplier) {
+        return pivot.setAngle(angleSupplier).withName("Pivot.SetAngleDynamic");
+    }
+
+    @Override
+    public void periodic() {
+        pivot.updateTelemetry();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        pivot.simIterate();
+    }
+}
