c30bd719be282157209d4f7896795d42b2119b15
Hovan-Y <362939308@gotvdsb.ca>
2026-01-31 10:43:58 -0500

    Hood Subsystem Set Up if needed

---
+++ Turret YAMS Testing/src/main/java/frc/robot/subsystems/HoodSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class HoodSubsystem extends SubsystemBase{
+    private Servo servo = new Servo(0);
+    
+    
+
+    public void setAngle(){
+        
+    }
+}
